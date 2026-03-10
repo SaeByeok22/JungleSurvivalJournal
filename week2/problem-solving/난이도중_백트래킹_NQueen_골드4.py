d7fc342910b06cd116a2670eaf40274fa548def7
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-10 10:49:54 +0900

    nqueen 타임아웃 수정

--- week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
+++ week2/problem-solving/난이도중_백트래킹_NQueen_골드4.py
@@ -2,7 +2,7 @@
 # 문제 링크: https://www.acmicpc.net/problem/9663
 
 N = int(input())
-count = 0
+# count = 0
 
 used_col = [False] * N
 used_diag1 = [False] * (2 * N - 1)
@@ -12,29 +12,32 @@ def nqueen(row) :
     global count
     
     if row == N:
-        count = count + 1
-        return
+        return 1
+    
+    total = 0
     
     for col in range(N):
         diag1 = row - col + (N - 1)
         diag2 = row + col
         
-        if used_col[col] == True:
-            continue
-        if used_diag1[diag1] == True:
-            continue
-        if used_diag2[diag2] == True:
-            continue
-    
-        used_col[col] = True
-        used_diag1[diag1] = True
-        used_diag2[diag2] = True
-        
-        nqueen(row + 1)
-        
-        used_col[col] = False
-        used_diag1[diag1] = False
-        used_diag2[diag2] = False
-    
-nqueen(0)
-print(count)
+        # if used_col[col]:
+        #     continue
+        # if used_diag1[diag1]:
+        #     continue
+        # if used_diag2[diag2]:
+        #     continue
+        if not used_col[col] and not used_diag1[diag1] and not used_diag2[diag2]:
+            
+            used_col[col] = True
+            used_diag1[diag1] = True
+            used_diag2[diag2] = True
+            
+            total += nqueen(row + 1)
+            
+            used_col[col] = False
+            used_diag1[diag1] = False
+            used_diag2[diag2] = False
+            
+    return total
+
+print(nqueen(0))
