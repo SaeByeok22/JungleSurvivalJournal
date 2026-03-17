0741e0592c09eec590e830a0c7becb0980c2286d
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-17 15:22:38 +0900

    이분탐색 완료

--- week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
+++ week3/problem-solving/난이도중_이분탐색_두용액_골드5.py
@@ -1,2 +1,16 @@
 # 이분탐색 - 두 용액 (백준 골드5)
-# 문제 링크: https://www.acmicpc.net/problem/2470
+N = int(input())
+a = sorted(map(int, input().split()))
+l, r = 0, len(a)-1
+ans = (a[l], a[r])
+
+while l < r:
+    s = a[l] + a[r]
+    if abs(s) < abs(ans[0] + ans[1]):
+        ans = (a[l], a[r])
+    if s > 0:
+        r -= 1
+    else:
+        l += 1
+
+print(*ans)
