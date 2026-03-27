9f072f18fd5f6f5961b3cf6e127453f1ea283563
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-27 22:03:01 +0900

    난이도 상 도전 완료

--- week2/problem-solving/난이도상_문자열_광고_플래4.py
+++ week2/problem-solving/난이도상_문자열_광고_플래4.py
@@ -1,2 +1,16 @@
 # 문자열 - 광고 (백준 플래4)
 # 문제 링크: https://www.acmicpc.net/problem/1305
+l = int(input())
+s = input().strip()
+
+pi = [0] * l
+j = 0
+
+for i in range(1, l):
+    while j > 0 and s[i] != s[j]:
+        j = pi[j - 1]
+    if s[i] == s[j]:
+        j += 1
+        pi[i] = j
+
+print(l - pi[-1])
