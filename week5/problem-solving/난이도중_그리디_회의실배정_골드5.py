442b47bd6e14b2c2e05ae3c3e3fd2065f3415720
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-28 14:54:18 +0900

    회의실 배정 끄읏

--- week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
+++ week5/problem-solving/난이도중_그리디_회의실배정_골드5.py
@@ -1,2 +1,21 @@
 # 그리디 - 회의실 배정 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/1931
+n = int(input())
+meetings = []
+
+for _ in range(n):
+    start, end = map(int, input().split())
+    meetings.append((start, end))
+
+# 끝나는 시간 우선, 같으면 시작 시간 기준 정렬
+meetings.sort(key=lambda x: (x[1], x[0]))
+
+count = 0
+last_end = 0
+
+for start, end in meetings:
+    if start >= last_end:
+        count += 1
+        last_end = end
+
+print(count)
