690dbeb61b0618f1a8e86a76308f9033cff883ca
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-30 22:03:56 +0900

    난이도 중 완료

--- week5/problem-solving/난이도중_DP_LCS_골드5.py
+++ week5/problem-solving/난이도중_DP_LCS_골드5.py
@@ -1,2 +1,25 @@
 # DP - LCS (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9251
+import sys
+input = sys.stdin.readline
+
+A = input().strip()
+B = input().strip()
+
+n, m = len(A), len(B)
+
+dp = [0] * (m + 1)
+
+for i in range(1, n + 1):
+    prev = 0  # dp[i-1][j-1]
+    for j in range(1, m + 1):
+        temp = dp[j]  # dp[i-1][j] 저장
+
+        if A[i - 1] == B[j - 1]:
+            dp[j] = prev + 1
+        else:
+            dp[j] = max(dp[j], dp[j - 1])
+
+        prev = temp
+
+print(dp[m])
--- week5/problem-solving/난이도중_DP_동전_골드5.py
+++ week5/problem-solving/난이도중_DP_동전_골드5.py
@@ -1,2 +1,24 @@
 # DP - 동전 (백준 골드5)
 # 문제 링크: https://www.acmicpc.net/problem/9084
+import sys
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    coins = list(map(int, input().split()))
+    M = int(input())
+
+    # m원을 만드는 방법 수
+    dp = [0] * (M + 1)
+
+    # 0원을 만드는 방법은 1개
+    dp[0] = 1
+
+    # 동전을 하나씩 사용하면서 경우의 수 누적
+    for coin in coins:
+        for money in range(coin, M + 1):
+            dp[money] += dp[money - coin]
+
+    print(dp[M])
