1fb59cd8ad24a17df1d7ff1a7031856e37edbee3
SaeByeok22 <icon0615@kyonggi.ac.kr>
2026-03-31 13:38:08 +0900

    멀티탭 스케줄링 완료

--- week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
+++ week5/problem-solving/난이도상_그리디_멀티탭스케줄링_골드1.py
@@ -1,2 +1,35 @@
 # 그리디 - 멀티탭 스케줄링 (백준 골드1)
 # 문제 링크: https://www.acmicpc.net/problem/1700
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+plug = []
+cnt = 0
+
+for i in range(k):
+    x = arr[i]
+
+    if x in plug:
+        continue
+
+    if len(plug) < n:
+        plug.append(x)
+        continue
+
+    idx = -1
+    far = -1
+
+    for p in plug:
+        if p not in arr[i + 1:]:
+            idx = p
+            break
+        next_use = arr[i + 1:].index(p)
+        if next_use > far:
+            far = next_use
+            idx = p
+
+    plug.remove(idx)
+    plug.append(x)
+    cnt += 1
+
+print(cnt)
